7cd36045a579417fd3ffd1a4ebf00abedad5803d
Alexander <sanya.kashin.2000@mail.ru>
2022-12-03 01:26:46 +0300

    ++

---
+++ dz_zad_64/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+
+int GetNumber(string x)
+{
+    Console.Write(x);
+    int userNumber = Convert.ToInt32(Console.ReadLine());;
+    return userNumber;
+}
+
+string rekursia(int number)
+{
+    string str = string.Empty;
+    if (number == 1) return number.ToString();
+    return number + " " + rekursia(number - 1);
+}
+
+int k = GetNumber("Введите число ");
+System.Console.WriteLine();
+System.Console.WriteLine(rekursia(k));
+
---
+++ dz_zad_66/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+
+int GetNumber(string msg)
+{
+    Console.WriteLine(msg);
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+    return userNumber;
+}
+
+void sumPrint(int number, int count, int sum)
+{
+    if (count <= number)
+    {
+        sum += count;
+        sumPrint(number, count + 1, sum);
+    }
+    else System.Console.WriteLine(sum);
+}
+
+
+
+
+int userNumberM = GetNumber("Введите число M:");
+int userNumberN = GetNumber("Введите число N:");
+int summ = 0;
+sumPrint(userNumberN, userNumberM, summ);
